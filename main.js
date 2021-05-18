41a76459fd52ffa33ca7a0dfca0440ede7c12be1
Tanvi <72029176+Tanvi2010@users.noreply.github.com>
2021-05-18 19:03:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -30,7 +30,9 @@ function setup() {
   video = createCapture(VIDEO);
   video.size(650, 500);
   video.hide();
+  video.parent('game_console');
   poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose',gotPoses);
 }
 
 function modelLoaded() {
